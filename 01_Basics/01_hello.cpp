b487a3e6fe404139cadb3ef5d9c0a833a172932d
raushan-19 <tsubodh778@gmail.com>
2025-12-26 10:40:31 +0530

    first commit

---
+++ 01_Basics/01_hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std ;
+int main () {
+    cout<<"hello world"<<"\trun baby run";
+    cout<<endl;     // use to change the line
+    cout<<"\n";     // use to change the line
+    cout<<"hello baby";
+}
---
+++ 01_Basics/02_practice.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std ;
+int main() {
+    cout<<4<<"\n4"<<endl<<4+3<<endl<<"4+3";
+} 

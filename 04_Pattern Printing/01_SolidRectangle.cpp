d0b0dc2072c601b83be1f8115d9dad756cdcadc5
raushan-19 <tsubodh778@gmail.com>
2026-01-03 12:20:42 +0530

    c++ files

---
+++ 04_Pattern Printing/01_SolidRectangle.cpp
@@ -0,0 +1,27 @@
+// Print the given pattern :- * * * * *
+//                            * * * * *
+//                            * * * * *
+//                            * * * * *
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, columns; // Declare two integer variables: rows and columns
+
+    cout << "Enter Rows : "; // Ask the user to enter number of rows
+    cin >> rows;             // Take input from user and store it in 'rows'
+
+    cout << "Enter Columns : "; // Ask the user to enter number of columns
+    cin >> columns;             // Take input from user and store it in 'columns'
+
+    for (int i = 1; i <= rows; i++) // Outer loop: runs from 1 to total number of rows
+    {
+        for (int j = 1; j <= columns; j++) // Inner loop: runs from 1 to total number of columns
+        {
+            cout << "*" << " "; // Print a star followed by a space (on the same line)
+        }
+
+        cout << endl; // Move to next line after finishing one full row
+    }
+}
---
+++ 04_Pattern Printing/03_alphabetSquare.cpp
@@ -0,0 +1,29 @@
+// Print the given pattern :- A B C D
+//                            A B C D
+//                            A B C D
+//                            A B C D
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, columns; // Declare two integer variables: rows and columns
+    char ch;
+
+    cout << "Enter Rows : "; // Ask the user to enter number of rows
+    cin >> rows;             // Take input from user and store it in 'rows'
+
+    cout << "Enter Columns : "; // Ask the user to enter number of columns
+    cin >> columns;             // Take input from user and store it in 'columns'
+
+    for (int i = 1; i <= rows; i++) // Outer loop: runs from 1 to total number of rows
+    {
+        for (int j = 97; j <= columns; j++) // Inner loop: runs from 1 to total number of columns
+        {
+            
+            cout << j = (char)ch << " "; // Print a star followed by a space (on the same line)
+        }
+
+        cout << endl; // Move to next line after finishing one full row
+    }
+}
